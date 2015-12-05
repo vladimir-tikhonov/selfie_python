0314cf3a3fd103076a8de006398542f4f1f982d3
Vladimir Tikhonov <vladimir@tikhonov.by>
2015-12-05 15:48:47 +0300

    Add login check to feed / post controllers

--- feed/views/feed_view.py
+++ feed/views/feed_view.py
@@ -1,7 +1,13 @@
 from django.views.generic import ListView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from posts.models.post import Post
 
-class FeedView(ListView):
+class FeedView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    login_url = '/login/'
     queryset = Post.objects.all()
     context_object_name = 'posts'
     template_name = 'feed/feed_page.html'
+
+    def test_func(self):
+        return self.request.user.role == 0
--- posts/views/post_view.py
+++ posts/views/post_view.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views.generic import View
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from posts.models.post import Post
 from posts.forms.post_form import PostForm
 
-class PostView(View):
+class PostView(UserPassesTestMixin, LoginRequiredMixin, View):
+    login_url = '/login/'
     form_class = PostForm
     template_name = 'posts/new.html'
 
@@ -23,3 +26,7 @@ class PostView(View):
             return HttpResponseRedirect(reverse('feed:index'))
         else:
             return render(request, self.template_name, {'form': form})
+
+    def test_func(self):
+        user_role = self.request.user.role
+        return user_role == 0 or user_role == 1
