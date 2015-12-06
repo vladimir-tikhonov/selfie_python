e8c2f95bf88d7a70ee5dee563d1ebef300253104
Vladimir Tikhonov <vladimir@tikhonov.by>
2015-12-06 14:31:59 +0300

    Allow to register as vip user

--- registration/forms/registration_form.py
+++ registration/forms/registration_form.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from user.models import User
 
 class RegistrationForm(UserCreationForm):
+    is_vip = forms.BooleanField(
+        label='Хотите зарегистрироваться как vip-пользователь?',
+    )
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        if self.cleaned_data['is_vip']:
+            user.is_active=False
+            user.role=1
+        if commit:
+            user.save()
+        return user
+
     class Meta:
         model = User
         fields = ("username", "email", )
